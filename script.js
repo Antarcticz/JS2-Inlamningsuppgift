07798797f1204e08b49fb851d28a78632b3913d0
Maximilian <maximilian_hedman@hotmail.com>
2023-02-02 06:44:12 +0100

    små fixat lite här och där

--- script.js
+++ script.js
@@ -1,80 +1,89 @@
-const API_URL = 'https://jsonplaceholder.typicode.com/todos?page=3&_limit=7';
-const BASE_URL = 'https://jsonplaceholder.typicode.com/todos';
+const API_URL = 'https://jsonplaceholder.typicode.com/todos?page=1337&_limit=7/';
+const BASE_URL = 'https://jsonplaceholder.typicode.com/todos/';
 
-const form = document.querySelector('#form')
+const form = document.querySelector('#form');
 const output = document.querySelector('#output');
 const btnAdd = document.querySelector('#add');
-const modal = document.getElementById("modal");
-const closeButton = document.getElementById('close-button');
+const modal = document.querySelector('#modal');
+const closeButton = document.querySelector('#close-button');
 
 let listArray = [];
 
 const getTodos = async () => {
-    const res = await fetch(API_URL)
-    const todos = await res.json()
+    const res = await fetch(API_URL);
+    const todos = await res.json();
 
-    console.log(todos)
+    console.log(todos);
 
     todos.forEach(todo => {
-        output.appendChild(createElement(todo))
-    })
-}
+        output.appendChild(createElement(todo));
+    });
+};
 
 getTodos();
 
 const createElement = (todo) => {
-    const card = document.createElement('div')
-    card.className = 'item'
-    card.id = todo.id
-    const title = document.createElement('p')
-    title.innerText = todo.title
+    const card = document.createElement('div');
+    card.className = 'item';
+    card.id = todo.id;
 
-    const btnDone = document.createElement('button')
-    btnDone.innerText = 'Done'
-    btnDone.className = 'btnDone btnStyle'
+    const title = document.createElement('p');
+    title.innerText = todo.title;
+
+    const btnDone = document.createElement('button');
+    btnDone.innerText = 'Done';
+    btnDone.className = 'btnDone btnStyle';
+
     if (todo.completed) {
-        title.classList.add('line-over')
-        btnDone.classList.add('completed')
-    }
+        title.classList.add('line-over');
+        btnDone.classList.add('completed');
+    };
+    
     btnDone.addEventListener('click', () => {
-        title.classList.toggle('line-over')
-        btnDone.classList.toggle('completed')
-    })
-    const btnUndo = document.createElement('button')
-    btnUndo.innerText = 'Undo'
-    btnUndo.className = 'btnUndo btnStyle'
+        title.classList.toggle('line-over');
+        btnDone.classList.toggle('completed');
+    });
+
+    const btnUndo = document.createElement('button');
+    btnUndo.innerText = 'Undo';
+    btnUndo.className = 'btnUndo btnStyle';
+    
     btnUndo.addEventListener('click', () => {
-        title.classList.remove('line-over')
-        btnDone.classList.remove('completed')
-    })
-    const btnDelete = document.createElement('button')
-    btnDelete.innerText = 'Delete'
-    btnDelete.className = 'btnDelete btnStyle'
+        title.classList.remove('line-over');
+        btnDone.classList.remove('completed');
+    });
+
+    const btnDelete = document.createElement('button');
+    btnDelete.innerText = 'Delete';
+    btnDelete.className = 'btnDelete btnStyle';
+
     btnDelete.addEventListener('click', () => {
+
         if (btnDone.classList.contains('completed')) {
-            btnDelete.parentElement.remove()
+            btnDelete.parentElement.remove();
 
             fetch(BASE_URL + "/" + btnDelete.parentElement.id, {
                 method: 'DELETE'
-            })
+            });
         }
         else {
-            function openModal() {
+            openModal = () => {
                 modal.style.display = 'block';
-            }
-        }
-        openModal()
+            };
+            openModal();
+        };
 
         closeButton.addEventListener('click', () => {
             modal.style.display = 'none';
         });
 
         window.addEventListener('click', (e) => {
+
             if (e.target == modal) {
                 modal.style.display = 'none';
-            }
-        })
-    })
+            };
+        });
+    });
 
     card.appendChild(title);
     card.appendChild(btnDone);
@@ -89,8 +98,8 @@ form.addEventListener('submit', e => {
 
     const newTodo = {
         title: document.querySelector('#text').value,
-        completed: false
-    }
+        completed: false,
+    };
 
     fetch(BASE_URL, {
         method: 'POST',
@@ -98,19 +107,22 @@ form.addEventListener('submit', e => {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
-    }).then((res) => res.json())
-        .then((todo) => {
-            const card = createElement(todo)
-            document.querySelector('#output').appendChild(card)
+    })
+    .then((res) => res.json())
 
-            console.log(todo);
-        })
+    .then((todo) => {
+        const card = createElement(todo);
+        document.querySelector('#output').appendChild(card);
+        console.log(todo);
+    });
 });
 
 empty = () => {
-    const input = document.querySelector('#text').value
+    const input = document.querySelector('#text').value;
+
     if (input.trim() == '') {
         alert(`Todo can't be empty`);
+
         return false;
-    }
+    };
 };
