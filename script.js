467ea0609634f6edbc126ebf1507e1598a25e93a
Maximilian <Maximilian_Hedman@hotmail.com>
2023-02-06 11:01:08 +0100

    glömde bort att fixa arrayen, nu är det fixat

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const API_URL = 'https://jsonplaceholder.typicode.com/todos?page=1337&_limit=7/';
+const API_URL = 'https://jsonplaceholder.typicode.com/todos?page=1&_limit=7/';
 const BASE_URL = 'https://jsonplaceholder.typicode.com/todos/';
+
 
 const form = document.querySelector('#form');
 const output = document.querySelector('#output');
@@ -7,21 +8,29 @@ const btnAdd = document.querySelector('#add');
 const modal = document.querySelector('#modal');
 const closeButton = document.querySelector('#close-button');
 
-let listArray = [];
+const todoArray = [];
 
 const getTodos = async () => {
     const res = await fetch(API_URL);
     const todos = await res.json();
 
-    console.log(todos);
-
     todos.forEach(todo => {
-        output.appendChild(createElement(todo));
+        todoArray.push(todo);
+
+        listTodos();
     });
 };
 getTodos();
 
-const createElement = (todo) => {
+const listTodos = () => {
+    output.innerHTML = '';
+    todoArray.forEach(todo => {
+        const todoElement = createTodoElement(todo);
+        output.appendChild(todoElement);
+    });
+};
+
+const createTodoElement = (todo) => {
     const card = document.createElement('div');
     card.className = 'item';
     card.id = todo.id;
@@ -56,18 +65,26 @@ const createElement = (todo) => {
     btnDelete.className = 'btnDelete btnStyle';
 
     btnDelete.addEventListener('click', () => {
+
         if (btnDone.classList.contains('completed')) {
             btnDelete.parentElement.remove();
 
             fetch(BASE_URL + "/" + btnDelete.parentElement.id, {
-                method: 'DELETE'
+                method: 'DELETE',
+            })
+            .then(res => {
+                if (res.ok) {
+                    btnDelete.parentElement.remove();
+
+                    const index = todoArray.findIndex((todo) => todo.id == todo);
+                    todoArray.splice(index, 1);
+
+                    console.log(todoArray);
+                };
             });
         }
         else {
-            openModal = () => {
-                modal.style.display = 'block';
-            };
-            openModal();
+            modal.style.display = 'block';
         };
 
         closeButton.addEventListener('click', () => {
@@ -108,10 +125,10 @@ form.addEventListener("submit", e => {
         .then((res) => res.json())
 
         .then((todo) => {
-            const card = createElement(todo);
-            output.appendChild(card);
-  
-            console.log(todo);
+            todoArray.push(todo);
+
+            const todoElement = createTodoElement(todo);
+            output.prepend(todoElement);
         });
         form.reset();
     };
